12bc7c8409893a4598bbaf5e2c8e00522d1b4f96
Lucaster85 <lucasdc87@gmail.com>
2020-06-07 08:17:33 -0300

    agrego ruta detalle de pelicula

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var moviesController = require('../controllers/moviesController');
 
-router.get('/', moviesController.list)
+router.get('/', moviesController.list);
+
+router.get('/:id', moviesController.detail);
 
 module.exports = router;
